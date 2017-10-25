107df03a48606043bb7f85b014b55bd37bd1dd96
danieljsmarda <danieljsmarda@gmail.com>
2017-10-24 22:29:59 -0500

    Fixed static file cache bug

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
- 
+import os
 # create a flask object (flask needs an object to represent the application)
 app = Flask(__name__) 
  
@@ -28,6 +28,55 @@ def authors():
 @app.route('/publishers/')
 def publishers():
 	return render_template('publishers.html')
+
+def static_file_hash(filename):
+  return int(os.stat(filename).st_mtime)
+
+
+# The following function handles caching problems
+# that prevent the local host from displaying
+# the updated static files.
+# Pasted directly from https://gist.github.com/Ostrovski/f16779933ceee3a9d181
+@app.url_defaults
+def hashed_url_for_static_file(endpoint, values):
+
+    if 'static' == endpoint or endpoint.endswith('.static'):
+
+        filename = values.get('filename')
+
+        if filename:
+
+            if '.' in endpoint:  # has higher priority
+
+                blueprint = endpoint.rsplit('.', 1)[0]
+
+            else:
+
+                blueprint = None  # can be None too
+
+
+
+            if blueprint:
+
+                static_folder = app.blueprints[blueprint].static_folder
+
+            else:
+
+                static_folder = app.static_folder
+
+
+
+            param_name = 'h'
+
+            while param_name in values:
+
+                param_name = '_' + param_name
+
+            values[param_name] = static_file_hash(os.path.join(static_folder, filename))
+
+            
+
+
 	
 # if app.py is run directly, i.e., as the main module, it will be assigned the value main # and if it's main go ahead and run the application. 
 # if this application is imported, then the __name__ is no longer __main__ and  
