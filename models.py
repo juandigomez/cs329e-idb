34a7cf5b02af1c80c69ab3d5b628563c057b641d
ZachQuinlan <z.quinlan96@utexas.edu>
2017-11-09 18:59:39 -0600

    Removed lengths of strings

--- models.py
+++ models.py
@@ -26,39 +26,39 @@ Base = declarative_base()
 
 class Book(Base):
 	__tablename__ = 'book'
-	title = Column(String(3000), nullable = False)
+	title = Column(String, nullable = False)
 	id = Column(Integer, primary_key = True, nullable = False)
-	isbn = Column(String(3000))
-	author = Column(String(3000))
-	publication_date = Column(String(3000))
-	google_id = Column(String(3000))
-	image = Column(String(3000))
-	description = Column(String(5000))
-	publisher = Column(String(1000))
+	isbn = Column(String)
+	author = Column(String)
+	publication_date = Column(String)
+	google_id = Column(String)
+	image = Column(String)
+	description = Column(String)
+	publisher = Column(String)
 	author_id = Column(Integer)
 	pub_id = Column(Integer)
 
 class Author(Base):
 	__tablename__ = 'author'
-	name = Column(String(3000), nullable = False)
+	name = Column(String, nullable = False)
 	id = Column(Integer, primary_key = True, nullable = False)
-	born = Column(String(3000))
-	education = Column(String(3000))
-	nationality = Column(String(3000))
-	description = Column(String(5000))
-	alma_mater = Column(String(3000))
-	wiki = Column(String(3000))
-	image = Column(String(3000))
+	born = Column(String)
+	education = Column(String)
+	nationality = Column(String)
+	description = Column(String)
+	alma_mater = Column(String)
+	wiki = Column(String)
+	image = Column(String)
 
 class Publisher(Base):
 	__tablename__ = 'publisher'
-	name = Column(String(3000))
-	wiki = Column(String(3000))
+	name = Column(String)
+	wiki = Column(String)
 	id = Column(Integer, primary_key = True, nullable = False)
-	description = Column(String(5000))
-	owner = Column(String(3000))
-	image = Column(String(3000))
-	website = Column(String(3000))
+	description = Column(String)
+	owner = Column(String)
+	image = Column(String)
+	website = Column(String)
 	
 
 
